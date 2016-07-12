90fcb2fc87a83d03d8bcf084cbccecd23cab229f
RomanVytak <roman.vytak@gmail.com>
2016-07-12 17:51:47 +0300

    first

---
+++ app/js/app/app.js
@@ -0,0 +1,248 @@
+(function() {
+
+	// var -------------------------------------------------------------------------------------------- //
+	var _body = $('body');
+
+	// check device
+	var isMobile = false;
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		isMobile = true;
+		_body.addClass('mobile');
+	} else {
+		_body.addClass('full-version');
+	}
+
+
+	// start! ----------------------------------------------------------------------------------------- //
+	window.addEventListener && window.addEventListener('DOMContentLoaded', function(){
+		document.body.className += ' dom-loaded';
+	});
+
+	// show site -------------------------------------------------------------------------------------- //
+	window.addEventListener && window.addEventListener('load', function(){
+		document.body.className += ' loaded';
+
+		$('.category .items-wrapper > div').masonry({
+			columnWidth: '.item'
+		});
+		$('.project .items-wrapper > div').masonry({
+			columnWidth: '.item'
+		});
+
+		// PROJECT PAGE
+		if($('body').hasClass('project-page')) {
+			closeScroll(); 		// блокувати скрол
+			openMaterial();		// відкрити лист матеріалів
+			numColorsElem(); 	// позиціонування елементів листа матеріалів
+			chooseSize(); 		// вибір розміру
+			chooser(); 			// 
+			if(!isMobile) {
+				elevateZoom(); 	// активація зуму
+				parralaxMaterial(); // активація паралаксу листа матеріалів
+			} else {
+				$('.owl-materials').owlCarousel({
+					loop: false,
+					nav: true,
+					responsiveClass: true,
+					responsive: {
+						0: { items: 1 },
+						461: { items: 2 },
+						641: { items: 3 },
+						854: { items: 4 },
+						1025: { items: 5 }
+					}
+				});
+				$('.owl-colors').owlCarousel({
+					loop: false,
+					nav: true,
+					responsiveClass: true,
+					responsive:{
+						0: { items: 3 },
+						461: { items: 4 },
+						641: { items: 5 },
+						854: { items: 7 },
+						1025: { items: 9 }
+					}
+				})
+			}
+		}
+
+
+
+
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// PROJECT PAGE ------------------------------------------------------------------------------ //
+
+	function closeScroll() {
+		var scrollCloseBlock = document.querySelector('.materials-wrapper');
+		function handler(event, delta, deltaX, deltaY) {
+			event.preventDefault();
+		};
+		Hamster(scrollCloseBlock).wheel(handler,false);
+	};
+
+	function elevateZoom() {
+		$('#zoom-project').elevateZoom({
+			zoomType: 'lens',
+			lensShape: 'round',
+			lensSize: 200
+		});
+	};
+
+	function parralaxMaterial() {
+		// визначаю ширину кожного елемента ТКАНИНА
+		var item = $('.materials-wrapper .materials .wrapper > div');
+		var num = item.length;
+		item.css({
+			'width': 100/num + '%',
+			'padding-top': 100/num + '%'
+		});
+		// визначаю ширину кожного елемента КОЛІР
+		var item = $('.materials-wrapper .colors .wrapper > div');
+		var num = item.length;
+		item.css({
+			'width': 100/num + '%',
+			'padding-top': 100/num + '%'
+		});
+
+		// go parralax materials
+		$('.materials-wrapper .materials .wrapper').parallax({
+			mouseport: $('.materials-wrapper .materials'),
+			yparallax: false,
+			xparallax: '100%'
+		});
+		// go parralax colors
+		$('.materials-wrapper .colors .wrapper').parallax({
+			mouseport: $('.materials-wrapper .colors'),
+			yparallax: false,
+			xparallax: '100%'
+		});
+	};
+
+	function openMaterial() {
+		$('.choice-material .elect-materials').on('click', function() {
+			if(!isMobile) {
+				$('body').toggleClass('show-materials');
+			} else {
+				$('body').toggleClass('show-materials');
+			};
+		});
+	};
+
+	function chooseSize() {
+		$('.project .small-sizes .size-1 span').on('click', function() {
+			$('.project .small-sizes .size-1 span').removeClass('active');
+			$(this).addClass('active');
+		});
+	};
+
+	function numColorsElem() {
+		var i,
+			elem = $('.project .choice-material .colors-wrapper span'),
+			num = elem.length,
+			calc = 180/num*2;
+		var fixedNum = calc;
+		for (i=0; i<num; i++) {
+			elem[i].style.webkitTransform = 'rotate(' + calc + 'deg' + ')';
+			elem[i].style.MozTransform = 'rotate(' + calc + 'deg' + ')';
+			elem[i].style.msTransform = 'rotate(' + calc + 'deg' + ')';
+			elem[i].style.OTransform = 'rotate(' + calc + 'deg' + ')';
+			elem[i].style.transform = 'rotate(' + calc + 'deg' + ')';
+			calc+=fixedNum;
+		};
+	};
+
+	function chooser() {
+
+		// вибір вигляду крісла
+		$('.project .small-images .item').on('click', function() {
+			var bigImage = $(this).attr('data-img');
+			$('.project .home-image img').attr('src', bigImage);
+			$('.project .home-image img').attr('data-zoom-image', bigImage);
+
+			// active class
+			$('.project .small-images > div > div').removeClass('active');
+			$(this).parent().addClass('active');
+
+			// re-initialize
+			if(!isMobile) {
+				$('#zoom-project').data('zoom-image', bigImage).elevateZoom({
+					zoomType: 'lens',
+					lensShape: 'round',
+					lensSize: 200
+				});
+			};
+		});
+
+		// вибір кольору на превюшці
+		var elem = $('.project .choice-material .colors-wrapper span span');
+		elem.on('click', function() {
+			elem.removeClass('active');
+			$(this).addClass('active');
+		});
+
+		// вибір матеріалу в попапі
+		$('.materials-wrapper .materials .item').on('click', function() {
+			$('.materials-wrapper .materials .item').removeClass('active');
+			$(this).addClass('active');
+			var material = $(this).attr('data-img');
+			$('.project .elect-materials').css('background', 'url(' + material + ')');
+		});
+
+		// вибір кольору в попапі
+		$('.materials-wrapper .colors .item').on('click', function() {
+			$('.materials-wrapper .colors .item').removeClass('active');
+			$(this).addClass('active');
+			var material = $(this).attr('data-img');
+			$('.project .elect-materials').css('background', 'url(' + material + ')');
+		});
+
+		// закрити попап
+		$('.materials-wrapper .close').on('click', function() {
+			$('body').removeClass('show-materials');
+		});
+	}
+
+
+
+
+
+
+
+
+})();
---
+++ app/js/app/bubbles.js
@@ -0,0 +1,169 @@
+// (function() {
+//     'use strict';
+    
+//     var camera, scene, renderer,
+//         container, stats, particle,
+//         winHalfX, winHalfY,
+//         height, width, fieldOfView,
+//         aspectRatio, nearPlane, farPlane,
+//         body, cameraZ, material,
+//         i = 0,
+//         count = 0,
+//         Tau = Math.PI * 2,
+//         mouseX = 0,
+//         mouseY = 0,
+//         amtX = 50,
+//         amtY = 50,
+//         sep = 100,
+//         spriteOpts = {},
+//         particles = [],
+//         //cssString = 'margin: 0; overflow: hidden;';
+
+//     function onDocumentReady() {
+//         body = document.body;
+//         //body.style.cssText = cssString;
+
+//         container = document.createElement('div');
+//         body.appendChild(container);
+
+//         height = window.innerHeight;
+//         winHalfY = height / 2;
+//         width = window.innerWidth;
+//         winHalfX = width / 2;
+//         fieldOfView = 75;
+//         aspectRatio = width / height;
+//         nearPlane = 1;
+//         farPlane = 2000;
+//         cameraZ = 650;
+
+//         rendererer(onRendererRenderered);
+//     }
+
+//     // renders the renderer 😎
+//     function rendererer(complete) {
+//         //lights (no lights!) camera, action!
+//         camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+//         camera.position.z = cameraZ;
+//         camera.position.y = 300;
+
+//         // aaaaaaaaand scene! (No wait, we're not done!)
+//         // aaaaaaaaand ten more minutes.
+//         scene = new THREE.Scene();
+
+//         //material settings
+//         spriteOpts = {
+//             color: 0xf6780a,
+//             program: function(ctx) {
+//                 ctx.beginPath();
+//                 ctx.arc(0, 0, 0.5, 0, Tau, true);
+//                 ctx.fill();
+//             }
+//         };
+
+//         material = new THREE.SpriteCanvasMaterial(spriteOpts);
+
+//         // Stop! particle time!
+//         for (var ix = 0, lx = amtX; ix < lx; ix++) {
+
+//             for (var iy = 0, ly = amtY; iy < ly; iy++) {
+//                 particle = particles[i++] = new THREE.Sprite(material);
+//                 particle.position.x = ix * sep - ((amtX * sep) / 2);
+//                 particle.position.z = iy * sep - ((amtY * sep) / 2);
+//                 scene.add(particle);
+//             }
+//         }
+
+//         //render the scene
+//         renderer = new THREE.CanvasRenderer();
+//         renderer.setClearColor(0xffffff, 1);
+//         renderer.setPixelRatio(window.devicePixelRatio);
+//         renderer.setSize(width, height);
+
+//         // append the rendered scene to the container
+//         container.appendChild(renderer.domElement);
+        
+//         // aaaaaaaaand Scene? (yes, scene!)
+//         if (complete) {
+//             complete();
+//         }
+//     }
+
+//     function onRendererRenderered() {
+//         // stats stats stats stats stats stats stats, eeeeerrryyybooodddaay! - Lil Jon, probably.
+//         stats = new Stats();
+//         stats.domElement.style.position = 'absolute';
+//         stats.domElement.style.top = stats.domElement.style.right = '0';
+//         container.appendChild(stats.domElement);
+
+//         // bind events
+//         document.addEventListener('', onDocumentMouseMove);
+//         document.addEventListener('touchstart', onDocumentTouchStart);
+//         document.addEventListener('touchmove', onDocumentTouchMove);
+//         window.addEventListener('resize', onWindowResize);
+
+//     }
+
+//     function onDocumentMouseMove(e) {
+//         mouseX = e.clientX - winHalfX;
+//         mouseY = e.clientY - winHalfY;
+//     }
+
+//     function onDocumentTouchStart(e) {
+//         if (e.touches.length === 1) {
+//             e.preventDefault();
+//             mouseX = e.touches[0].pageX - winHalfX;
+//             mouseY = e.touches[0].pageY - winHalfY;
+//         }
+//     }
+
+//     function onDocumentTouchMove(e) {
+//         if (e.touches.length === 1) {
+//             e.preventDefault();
+//             mouseX = e.touches[0].pageX - winHalfX;
+//             mouseY = e.touches[0].pageY - winHalfY;
+//         }
+//     }
+
+//     function onWindowResize() {
+//         // update variables to new values
+//         height = window.innerHeight;
+//         winHalfY = height / 2;
+//         width = window.innerWidth;
+//         winHalfX = width / 2;
+
+//         camera.aspect = width / height;
+//         camera.updateProjectionMatrix();
+//         renderer.setSize(width, height);
+//     }
+
+//     function animate() {
+//         requestAnimationFrame(animate);
+
+//         update();
+//         stats.update();
+//     }
+
+//     function update() {
+//         //camera.position.x += (mouseX - camera.position.x) * 0.05;
+//         //camera.position.y += (mouseY - camera.position.y) * 0.05;
+//         camera.lookAt(scene.position);
+        
+//         i = 0;
+//         for (var ix = 0, lx = amtX; ix < lx; ix++) {
+
+//             for (var iy = 0, ly = amtY; iy < ly; iy++) {
+//                 particle = particles[i++];
+//                 particle.position.y = (Math.sin((ix + count) * 0.3) * 50) + (Math.sin((iy + count) * 0.5) * 50);
+//                 particle.scale.x = particle.scale.y = (Math.sin((ix + count) * 0.3) ) * 4 + (Math.sin((iy + count) * 0.5) ) * 4;
+//                 scene.add(particle);
+//             }
+//         }
+
+//         renderer.render(scene, camera);
+//         count += 0.1;
+//     }
+
+//     document.addEventListener('DOMContentLoaded', onDocumentReady);
+//     document.addEventListener('DOMContentLoaded', animate);
+
+// })();
