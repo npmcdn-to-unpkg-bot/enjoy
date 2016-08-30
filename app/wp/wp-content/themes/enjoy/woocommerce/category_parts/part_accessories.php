e09359aea430fce315f38ada865a1397f12e1b51
RomanVytak <roman.vytak@gmail.com>
2016-08-30 16:46:00 +0300

    new

--- app/wp/wp-content/themes/enjoy/woocommerce/category_parts/part_accessories.php
+++ app/wp/wp-content/themes/enjoy/woocommerce/category_parts/part_accessories.php
@@ -1,3 +1,21 @@
-	<!-- interactive -->
-	<div class="interactive">
-<div class="image" style="background:url('<?php bloginfo('template_directory'); ?>/pictures/interactive/image-2.jpg') no-repeat center;"></div>	</div>
+<div class="interactive accesories">
+
+	<!-- image -->
+	<div class="image" style="background:url('<?php bloginfo('template_directory'); ?>/pictures/interactive/accesories.jpg') no-repeat center/cover;"></div>
+
+	<!-- text -->
+<!-- 	<div class="text-wrapper">
+		<div class="tab">
+			<div class="cell">
+				<div class="wrapper">
+					<p class="category">Original style</p>
+					<h1 class="title">
+						Крісло-груша
+						<span>For everyday</span>
+					</h1>
+					<p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam non nobis magnam, deleniti asperiores odio a, eum saepe eaque repellat libero nemo, voluptate sequi quaerat aperiam explicabo enim perspiciatis provident. Expedita quia, repudiandae amet et!</p>
+				</div>
+			</div>
+		</div>
+	</div> -->
+</div>
--- app/wp/wp-content/themes/enjoy/woocommerce/category_parts/part_paradise.php
+++ app/wp/wp-content/themes/enjoy/woocommerce/category_parts/part_paradise.php
@@ -1,1 +1,21 @@
-part_paradise
+<div class="interactive paradise">
+
+	<!-- image -->
+	<div class="image" style="background:url('<?php bloginfo('template_directory'); ?>/pictures/interactive/paradise.jpg') no-repeat center/cover;"></div>
+
+	<!-- text -->
+	<div class="text-wrapper">
+		<div class="tab">
+			<div class="cell">
+				<div class="wrapper">
+					<p class="category">for outdoor</p>
+					<h1 class="title">
+						Шезлонги
+						<span>paradise</span>
+					</h1>
+					<p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam non nobis magnam, deleniti asperiores odio a, eum saepe eaque repellat libero nemo, voluptate sequi quaerat aperiam explicabo enim perspiciatis provident. Expedita quia, repudiandae amet et!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
